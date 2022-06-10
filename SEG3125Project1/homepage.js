b162f7aad20422b9e5b4aa16ebb440bafb67020a
unknown <zcui014@uottawa.ca>
2022-06-10 00:01:16 -0400

    finish project 1

---
+++ SEG3125Project1/homepage.js
@@ -0,0 +1,112 @@
+
+function changeOptions(){
+    let text = document.getElementById('category').value;
+    let os = ['Dr.Lee', 'Dr.Noordam'];
+    console.log(text);
+    let or = ['Dr.Lee'];
+    let ph = ['Dr.Noordam', 'Mr.Dsouza'];
+    let option = document.getElementById('expert')
+    option.childNodes.forEach(op =>{
+        option.options[0] = null;
+    })
+    if (text == 'Oral Surgeons'){
+        os.forEach(ex => {
+            option.options.add(new Option(ex,ex));
+        })
+    }
+    else if (text == 'Prevent Hygiene') {
+        ph.forEach(ex => {
+            option.options.add(new Option(ex,ex));
+        })
+    }
+    else if (text == 'Orthodontics'){
+        or.forEach(ex => {
+            option.options.add(new Option(ex,ex));
+        })
+    }
+    else{
+        option.options = [];
+    }
+
+}
+
+
+document.addEventListener("click", event => {
+    const cDom = document.getElementById("booking");
+    const tDom = event.target;
+    if (cDom.style.display === 'block'){
+        console.log("executed2")
+        if (cDom == tDom || cDom.contains(tDom) || tDom == document.getElementById('primeBtn') || tDom == document.getElementById('primeBtn2')|| tDom == document.getElementById('secBtn')) {
+            // ...
+        } else {
+            cDom.style.display = "none";
+            document.getElementsByTagName('body').item(0).style.overflowY = 'auto';
+            document.getElementById('overlay').style.cssText = '';
+        }
+    }
+
+});
+
+document.addEventListener("click", event => {
+    const cDom = document.getElementById("confirming");
+    const tDom = event.target;
+    if (cDom.style.display === 'block'){
+        console.log("executed2")
+        if (cDom == tDom || cDom.contains(tDom) || tDom == document.getElementById('scheduleBtn')) {
+            // ...
+        } else {
+            cDom.style.display = "none";
+            document.getElementsByTagName('body').item(0).style.overflowY = 'auto';
+            document.getElementById('overlay').style.cssText = '';
+        }
+    }
+
+});
+function raiseBookingPage(){
+    console.log("executed")
+    document.getElementById("booking").style.display = 'block';
+    document.getElementsByTagName('body').item(0).style.overflowY = 'hidden'
+    document.getElementById('overlay').style.cssText = 'z-index = -1;background: black; opacity: 0.7; left: 0; top: 0; position: fixed; width: 100%; height: 100%;'
+
+}
+function visibleByName(text){
+    let services = ['hygieneText', 'oralText', 'orthogText'];
+    services.forEach(service =>{
+        if (service !== (text +'Text')) {
+
+            document.getElementById(service).style.display = 'none';
+        }
+        else {
+            document.getElementById(service).style.display = 'block';
+        }
+    })
+}
+
+window.onload = function() {
+    document.getElementById("hygiene").focus();
+    document.getElementById('datePickerId').setAttribute("min",new Date().toLocaleDateString('en-ca'));
+    const picker = document.getElementById('datePickerId');
+    const timePicker = document.getElementById('timePickerId');
+    picker.addEventListener('input', function (e){
+        const day = new Date(picker.value).getUTCDay();
+        if ([6,0].includes(day)) {
+            timePicker.setAttribute("min", "00:00");
+            timePicker.setAttribute("max", "00:00");
+        }
+    })
+
+}
+
+function closeOverlay(){
+    document.getElementById("booking").style.display = 'none';
+    document.getElementById('confirming').style.display = 'none';
+    document.getElementById('overlay').style.cssText = '';
+}
+
+function goNextOverlay(){
+    document.getElementById("booking").style.display = 'none';
+    document.getElementById('confirming').style.display = 'block';
+    document.getElementsByTagName('body').item(0).style.overflowY = 'auto';
+}
+
+
